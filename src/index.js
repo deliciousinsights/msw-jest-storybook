1d582ad54cd27fb13e2913414fc4b5f94c442108
Maxime Bréhin <maxime@delicious-insights.com>
2021-08-26 10:33:17 +0200

    Setup MSW service worker on dev

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import Posts from './components/Posts'
 
-buildHTML()
+let appReady = Promise.resolve()
+
+// Enable API mocking only in development.
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  appReady = worker.start({
+    serviceWorker: {
+      /**
+       * Use a custom Service Worker script URL to resolve
+       * the mock worker served by Codesandbox.
+       * @note You DO NOT need this in your application.
+       * @see https://mswjs.io/docs/api/setup-worker/start#serviceworker
+       */
+      url: '/mockServiceWorker.js',
+    },
+  })
+}
+
+/**
+ * Use deferred application mounting in order to work in a sandbox.
+ * You MAY NOT need this in your application.
+ * @see https://mswjs.io/docs/recipes/deferred-mounting
+ */
+appReady.then(() => buildHTML())
 
 function buildHTML() {
   const app = document.getElementById('app')
