49982bbe095b12db12ea83d84e4059d08f90c3d2
Maxime Bréhin <maxime@delicious-insights.com>
2021-08-26 10:59:10 +0200

    Load bundle in index.html

--- src/index.js
+++ src/index.js
@@ -10,19 +10,8 @@ async function buildHTML() {
   )
 
   document.getElementById('app').innerHTML = `
-  <div class="mdl-grid">
-    <div class="mdl-cell mdl-cell--10-col mdl-cell--12-col-tablet">
-      <h1>Jest and MSW sandbox</h1>
-      <p>
-        Please have a look at the source code to know more about the way it works.
-      </p>
-      <p>
-        The data you can see are loaded from <a href="https://jsonplaceholder.typicode.com/guide/">jsonplaceholder.typicode.com</a>.
-      </p>
-      <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
-        <tbody>${postsRows.join('')}</tbody>
-      </table>
-    </div>
-  </div>
+    <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+      <tbody>${postsRows.join('')}</tbody>
+    </table>
   `
 }
