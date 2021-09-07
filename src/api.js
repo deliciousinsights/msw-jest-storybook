eb3d06335d1dd26947f397b9ff006f7d3c32e2e7
Maxime Bréhin <maxime@delicious-insights.com>
2021-09-07 11:57:58 +0200

    Update API: limit to `getPost`, `getPosts`, `createPost`

--- src/api.js
+++ src/api.js
@@ -3,42 +3,16 @@ import fetch from 'cross-fetch'
 export const POST_BASE_URL = 'https://jsonplaceholder.typicode.com/posts'
 
 export function getPosts() {
-  return jsonFetch(POST_BASE_URL)
+  return fetch(POST_BASE_URL).then((response) => response.json())
 }
 
 export function getPost(id) {
-  return jsonFetch(`${POST_BASE_URL}/${id}`)
+  return fetch(`${POST_BASE_URL}/${id}`).then((response) => response.json())
 }
 
-export function createPost(data) {
-  return post(POST_BASE_URL, data)
+export function createPost(body) {
+  return fetch(POST_BASE_URL, {
+    body,
+    method: 'post',
+  }).then((response) => response.json())
 }
-
-export function deletePost(id) {
-  return drop(`${POST_BASE_URL}/${id}`)
-}
-
-export function patchPost(data) {
-  return drop(`${POST_BASE_URL}/${data.id}`, data)
-}
-
-// -----
-// Utils
-// -----
-function jsonFetch(url, body = {}) {
-  return fetch(url, body).then((response) => response.json())
-}
-
-function makeJSONCall(name, method = name.toUpperCase()) {
-  const result = function (url, body, options = {}) {
-    return jsonFetch(url, { body, method, ...options })
-  }
-  result.displayName = name
-  return result
-}
-
-export const drop = makeJSONCall('drop', 'DELETE')
-export const get = makeJSONCall('get')
-export const patch = makeJSONCall('patch')
-export const post = makeJSONCall('post')
-export const put = makeJSONCall('put')
